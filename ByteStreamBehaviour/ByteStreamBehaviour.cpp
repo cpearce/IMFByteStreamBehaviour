ced950dd95e302c20dc1d90cc487487c37bd946e
Chris Pearce <cpearce@mozilla.com>
2013-02-01 11:53:21 +1300

    Document, fix refcnt on ReadCallback. Add public domain disclaimer.

--- ByteStreamBehaviour/ByteStreamBehaviour.cpp
+++ ByteStreamBehaviour/ByteStreamBehaviour.cpp
@@ -1,5 +1,40 @@
-// ByteStreamBehaviour.cpp : Defines the entry point for the console application.
+// ByteStreamBehaviour.cpp
 //
+// This program creates a IMFByteStream and test various edge cases in order
+// to determine its behaviour. This is so that I can figure out how to copy
+// its behaviour in my own IMFByteStream implementation.
+//
+// Chris Pearce, 1 Feb 2013.
+
+/**
+
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <http://unlicense.org/>
+
+**/
+
 
 #include "stdafx.h"
 
@@ -65,7 +100,7 @@ class ReadCallback : public IMFAsyncCallback
 {
 public:
 
-  ReadCallback () : m_cRef(1) {
+  ReadCallback () : m_cRef(0) {
     AutoLock autoLock(lock);
     result = nullptr;
     mReadEvent = CreateEvent(NULL, TRUE, FALSE, L"readEvent");
@@ -74,13 +109,11 @@ public:
       printf("CreateEvent failed\n");
       exit(-1);
     }
-    printf("ReadCallback CTOR\n");
   }
   
   virtual ~ReadCallback() {
     AutoLock autoLock(lock);
     CloseHandle(mReadEvent);
-    printf("ReadCallback DTOR\n");
   }
 
   STDMETHODIMP QueryInterface(REFIID riid, void** ppv){
@@ -104,7 +137,6 @@ public:
   }
 
   STDMETHODIMP GetParameters(DWORD* pdwFlags, DWORD* pdwQueue) {
-    // Implementation of this method is optional.
     return E_NOTIMPL;
   }
 
@@ -343,6 +375,8 @@ int _tmain(int argc, _TCHAR* argv[])
   IMFByteStreamPtr bs;
 
   /*
+  // Creates an IMFByteStream wrapping an IStream. This doesn't have the same
+  // behaviour as WMF's network reading IMFByteStream, don't try to copy it.
   const int bufLen = 1000;
   BYTE buf[bufLen ];
   memset(buf, 0xaa, bufLen);
